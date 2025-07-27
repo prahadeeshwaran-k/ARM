d423caf18c9e3f1b71ffb450fd614b02c4e60878
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-07-27 22:49:56 +0530

    LeftShiftLed

--- Assignment/2_Right_Shift_8_LED/main.c
+++ Assignment/2_Right_Shift_8_LED/main.c
@@ -11,7 +11,7 @@ int main(){
 	IOSET0 = 0XFF;//For active Low LEDs, set all the 8 LED  to off state 
 	
 	for(i=0;i<N;++i){
-		for(j=0;j<=7;++j){
+		for(j=7;j>=0;--j){
 			IOCLR0 = (1<<j);//Turning ON the LED by setting the j-th pin to '0' (active low)
 			delay_ms(500);  // Delay For 500 ms
 			IOSET0 = (1<<j);//Turning OFF the LED by setting the j-th pin to '1' (active low)
