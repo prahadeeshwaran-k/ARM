b4265172d3b4f0b0aacb4b0cc6abfb33cd6281a2
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-07-28 23:38:21 +0530

    4BUTTON FIX

--- Assignment/4_Multi_Button_Input_LED/main.c
+++ Assignment/4_Multi_Button_Input_LED/main.c
@@ -1,20 +1,21 @@
 #include<LPC21XX.H>
-#include<Delay.h>
+#include"Delay.h"
 
 int DigitalRead(int pin){
-	return (IOPIN0>>button)&1;
+	return ((IOPIN0>>pin)&1);
 }
 
 void PressButton(int led, int button){
-	if((DigitalRead(led)==0){
+	if((DigitalRead(button)==0)){
 			IOCLR0 = 1<<led;
 		}else{
 			IOSET0 = 1<<led
 		}
 }
 
-int main(){
+int main(){													  
 	IODIR0=0x0F;
+	IOSET0=0x0F;
 	while(){
 		PressButton(0,4);
 		PressButton(1,5);
