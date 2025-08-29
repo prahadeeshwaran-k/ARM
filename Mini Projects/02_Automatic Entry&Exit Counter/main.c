15ccb008bd384d693f26018a5ccfe1485fd75a60
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-08-29 19:54:22 +0530

    fix

--- Mini Projects/02_Automatic Entry&Exit Counter/main.c
+++ Mini Projects/02_Automatic Entry&Exit Counter/main.c
@@ -1,4 +1,6 @@
 #include<LPC21XX.H>
+#include"Delay.h"
+#include"UART.h"
 
 #define EXT_INT0 14
 #define EXT_INT1 15
@@ -18,7 +20,7 @@ volatile unsigned char directionFlag2;
 volatile unsigned char CountFlag1[2]= {0,0};
 volatile unsigned char CountFlag2[2]= {0,0};
 
-volatile unsigned int Humans;
+volatile unsigned char Humans = 0;
 
 void Config_Interrupt(void);
 
@@ -54,14 +56,24 @@ void Non_Vectored_Isr(void)__irq {
 	// For Door 2 Logic
 	case (1 << EXT_INT1):
 		EXTINT = 0x02;      // clear EINT1
-		IOSET0 = LED2;
-		IOCLR0 = LED2;
+		if(directionFlag2== 0) { //when directionFlag1 = 0 means it occurs 1st
+			sensorFlag2[0] = 1;
+			directionFlag2 = 1;
+		} else {
+			sensorFlag2[0] = 0;
+		}
+		CountFlag2[0] = 1;
 		break;
 
 	case (1 << EXT_INT2):
 		EXTINT = 0x04;      // clear EINT2
-		IOSET0 = LED3;
-		IOCLR0 = LED3;
+		if(directionFlag2 == 0) { //when directionFlag1 = 0 means it occurs 1st
+			sensorFlag2[1] = 1;
+			directionFlag2 = 1;
+		} else {
+			sensorFlag2[1] = 0;
+		}
+		CountFlag2[1] = 1;
 		break;
 	}
 
@@ -69,24 +81,36 @@ void Non_Vectored_Isr(void)__irq {
 }
 
 
-
 int main() {
-	IODIR0 |= (LED1 | LED2 | LED3 | LED4);
-	PINSEL0 = 0xA0000000;   // P0.14 -> EINT1, P0.15 -> EINT2
-	PINSEL1 = 0x00000301;		// P0.16 -> EINT0, P0.30 -> EINT3
+	IODIR0  |= (LED1 | LED2 | LED3 | LED4);
+	PINSEL0 |= 0xA0000000;   // P0.14 -> EINT1, P0.15 -> EINT2
+	PINSEL1 |= 0x00000301;		// P0.16 -> EINT0, P0.20 -> EINT3
 
 	Config_Interrupt();
+	UART0_CONFIG();
+	UART0_STR("Automatic Person Counter");
+	UART0_STR("\r\n");
 
 	EXTMODE = 0x0f;
 	EXTPOLAR = 0x0f;
 
 	VICIntEnable = (1<<EXT_INT0) | (1<<EXT_INT1) | (1<<EXT_INT2) | (1<<EXT_INT3) ;
 	while(1) {
+		
 		if(CountFlag1[0] && CountFlag1[1]) {
-			if(sensorFlag1[0])
+			if(sensorFlag1[0]){
 				Humans++;
-			if(sensorFlag1[1])
-				Humans--;
+				UART0_STR("in door 1 +One Person Entered: TotalPersons ");
+				UART0_TX(Humans);
+				UART0_STR("\r\n");
+			}
+
+			if(sensorFlag1[1]){
+				if(humans>0){Humans--}
+				UART0_STR("in door 1 -One Person Exited: TotalPersons ");
+				UART0_TX(Humans);
+				UART0_STR("\r\n");
+			}
 
 			//Reset All The Flags
 			CountFlag1[0] = 0;
@@ -95,6 +119,30 @@ int main() {
 			sensorFlag1[0] = 0;
 			sensorFlag1[1] = 0;
 		}//1st if Ends
+		
+		if(CountFlag2[0] && CountFlag2[1]) {
+			if(sensorFlag2[0]){
+				Humans++;
+				UART0_STR("+One Person Entered: TotalPersons ");
+				UART0_TX(Humans);
+				UART0_STR("\r\n");
+				}
+			
+			if(sensorFlag2[1]){
+				if(humans>0){Humans--}
+				UART0_STR("- One Person Exited: TotalPersons ");
+				UART0_TX(Humans);
+				UART0_STR("\r\n");
+				}
+
+			//Reset All The Flags
+			CountFlag2[0] = 0;
+			CountFlag2[1] = 0;
+			directionFlag2 = 0;
+			sensorFlag2[0] = 0;
+			sensorFlag2[1] = 0;
+		}//2st if Ends
+		
 	}//SuperLoop
 }//Main Function
 
@@ -102,3 +150,5 @@ void Config_Interrupt(void) {
 	VICIntSelect = 0;
 	VICDefVectAddr = (int)Non_Vectored_Isr;
 }
+
+
